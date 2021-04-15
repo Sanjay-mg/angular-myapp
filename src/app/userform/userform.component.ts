94ed7a7071044850956f59fc0693cfa7a84cd6a7
unknown <ibmjavanh17@iiht.tech>
2021-04-15 14:04:08 +0530

    added tabular display

--- src/app/userform/userform.component.ts
+++ src/app/userform/userform.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../User';
+
 
 @Component({
   selector: 'app-userform',
   templateUrl: './userform.component.html',
   styleUrls: ['./userform.component.css']
 })
-export class UserformComponent implements OnInit {
+export class UserformComponent implements OnInit { // controller
   title:string = 'userform';
-  firstname:string='Ram';
+  user:User=new User(); // model - stores all form data
+  userArray:User[]=[];
   constructor() { }
-
+  save(){
+    this.userArray.push(Object.assign({},this.user));
+    console.log('working...');
+    console.log(this.user.firstname);
+    console.log(this.user.age);
+  }
   ngOnInit(): void {
   }
 
