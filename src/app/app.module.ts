5acd16096a1f7eeea0089d0a3bcd8af112932588
unknown <ibmjavanh17@iiht.tech>
2021-04-20 10:42:03 +0530

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,29 @@
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 import {FormsModule} from '@angular/forms'
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { UserformComponent } from './userform/userform.component';
+import { SearchComponent } from './search/search.component';
+
+const appRoutes: Routes = [
+  { path: '', component: UserformComponent },
+  { path: 'search', component: SearchComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    UserformComponent
+    UserformComponent,
+    SearchComponent
   ],
   imports: [
-    BrowserModule, FormsModule, HttpClientModule,
+    BrowserModule, FormsModule, HttpClientModule,RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
